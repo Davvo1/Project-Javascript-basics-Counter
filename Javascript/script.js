3074775608d6c9256176e075e8ebce21eba8cf70
Davvo1 <davidebonardi15@gmail.com>
2023-03-04 10:36:19 +0100

    Created buttons dynamically

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,31 @@
 let output = document.querySelector("#output");
-let add = document.querySelector("#add");
-let subtract = document.querySelector("#subtract");
 let clear = document.querySelector("#clear");
+const buttonHolder = document.querySelector("#buttonHolder");
 
+let buttonPlus = document.createElement('button');
+buttonPlus.classList.add("button");
+buttonPlus.id = "add";
+buttonPlus.innerText = "+"; 
+buttonHolder.append(buttonPlus);
 
+let buttonMinus = document.createElement('button');
+buttonMinus.classList.add("button");
+buttonMinus.id = "subtract";
+buttonMinus.innerText = "-"; 
+buttonHolder.append(buttonMinus);
 
+let buttonClear = document.createElement('button');
+buttonClear.classList.add("button");
+buttonClear.id = "clear";
+buttonClear.innerText = "Clear"; 
+buttonHolder.append(buttonClear);
 
-add.addEventListener("click", function(){
+buttonPlus.addEventListener("click", function(){
  let result = Number(output.innerText) + 1;
  output.innerText = result;
 });
 
-subtract.addEventListener("click", function(){
+buttonMinus.addEventListener("click", function(){
     let result = Number(output.innerText) - 1;
     
     if (result < 0) {
@@ -21,7 +35,7 @@ subtract.addEventListener("click", function(){
     output.innerText = result;
    });
 
-clear.addEventListener("click", function(){
+buttonClear.addEventListener("click", function(){
  output.innerText = "0";
    });
 
